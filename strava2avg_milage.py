d77f820710c04280900de09ed0c8057c0ef67f16
Jason Downing <jason@downing.io>
2021-09-04 23:18:08 -0400

    looks like I avged 90 miles a week over the last 8 weeks if i did my math right. wow.

--- strava2avg_milage.py
+++ strava2avg_milage.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import sys
 from datetime import datetime
+from datetime import timedelta
 
 """
     This script will calculate the average milage for a given strava "activities.csv" export
     The average milage will be per week, over the last 8 weeks
+
+    TODO: make an input "weeks", then do the calculation based on that
+    TODO: then figure out the average for various week periods, like 2 vs 4 vs 8 vs 20 weeks
 """
 
 fields = ["Activity Date", "Distance"]
@@ -31,8 +35,13 @@ except:
     # Ye this is apparently not recommended but it doesn't quit the Idle shell on my Windows box so...
     raise SystemExit
 
-# Collect distances within the last 8 weeks
+# Collect distances within the last 8 weeks, so use a time obj that's set to a day
+# 8 weeks ago for comparsion later
+td = timedelta(days=56)
+dtEight = datetime.today() - td
+print(dtEight)
 
+milageSum = 0
 
 for index, row in activitiesDF.iterrows():
 
@@ -40,9 +49,18 @@ for index, row in activitiesDF.iterrows():
     distance = float(row["Distance"]) / 1.609
     distanceFm = "{:.2f}". format(distance)
 
-    print("%s: %s miles" % (dateStr, distanceFm))
-
-    
-
+    # Parse datetime
     datetimeObj = datetime.strptime(dateStr, '%b %d, %Y, %I:%M:%S %p')
-    print(datetimeObj)
+
+    # If datetime is in the past 8 weeks range, sum up it's milage
+    if datetimeObj > dtEight:
+
+        milageSum += float(distanceFm)
+
+        # Debug prints
+        print("%s: %s miles | milageSum = %.2f" % (dateStr, distanceFm, milageSum))
+        print(datetimeObj)
+
+# Moment of truth: divide by 8, get the average milage over the last 8 weeks!
+avgMilage = milageSum / 8
+print("Average milage over the last 8 weeks was: %.2f miles" % avgMilage)
