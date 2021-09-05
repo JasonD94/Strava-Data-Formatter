45029c5b781b8d562e05125783069e0b812e11f3
Jason Downing <jason@downing.io>
2021-09-04 22:41:09 -0400

    initial reading of strava activities file

---
+++ strava2avg_milage.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import sys
+
+"""
+    This script will calculate the average milage for a given strava "activities.csv" export
+    The average milage will be per week, over the last 5 weeks
+"""
+
+fields = ["Activity Date", "Distance"]
+
+filePth = None
+
+try:
+    filePth = sys.argv[1]
+except:
+    print("No cmd args found! Running with default activities.csv file instead.\n")
+
+# activities.csv is the default export name for LinkedIn Messages
+activitiesDF = None
+
+try:
+    if filePth:
+        print(filePth)
+        activitiesDF = pd.read_csv(filePth, usecols=fields)
+    else:
+        activitiesDF = pd.read_csv("csv/activities.csv", usecols=fields)
+except:
+    print("\n\n!!!Error: you need to provide me with a activities.csv file from LinkedIn for me to parse!!\n\n")
+
+    # Ye this is apparently not recommended but it doesn't quit the Idle shell on my Windows box so...
+    raise SystemExit
+
+# Collect distances within the last 5 weeks
+
+
+for index, row in activitiesDF.iterrows():
+
+    date = str(row["Activity Date"])
+    distance = float(row["Distance"]) / 1.609
+    distanceFm = "{:.2f}". format(distance) 
+
+    print("%s: %s miles" % (date, distanceFm))
+
